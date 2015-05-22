9784582605f2d9e08b0f7e25051b434d657b9c0f
tgloeggl <tgloeggl@uos.de>
2015-05-22 10:33:56 +0200

    coding conventions -> change <?php to <?

--- views/registrations/_create_and_register.php
+++ views/registrations/_create_and_register.php
@@ -10,8 +10,8 @@ $termsOfServiceUrl = PluginEngine::getLink($plugin, array(), 'registrations/term
 $privacyPolicyUrl = PluginEngine::getLink($plugin, array(), 'registrations/privacy_policy');
 ?>
 <form class="signup" method="post" action="<?= $controller->url_for('registrations/create') ?>">
-    <?php foreach ($fields as $field): ?>
-        <?php if (is_array($field) && $field['fieldName'] === 'accept_tos'): ?>
+    <? foreach ($fields as $field): ?>
+        <? if (is_array($field) && $field['fieldName'] === 'accept_tos'): ?>
             <input type="checkbox"
                 name="accept_tos"
                 id="mooc_sign_up_terms_of_service"
@@ -20,39 +20,39 @@ $privacyPolicyUrl = PluginEngine::getLink($plugin, array(), 'registrations/priva
                 Ich akzeptiere die <a href="<?= $termsOfServiceUrl ?>" target="_blank">Nutzungsbedingungen</a>
                 und die <a href="<?= $privacyPolicyUrl ?>" target="_blank">Datenschutzerklärung</a>.
             </label>
-        <?php elseif (is_array($field)): ?>
+        <? elseif (is_array($field)): ?>
             <label for="mooc_sign_up_<?= $field['fieldName'] ?>"<?= $field['required'] ? ' class="required"' : '' ?>>
                 <?= $field['label'] ?>
-                <?php if ($field['required']): ?>
+                <? if ($field['required']): ?>
                     *
-                <?php endif ?>
+                <? endif ?>
             </label>
-            <?php if (is_array($field['choices'])): ?>
+            <? if (is_array($field['choices'])): ?>
                 <select name="<?= $field['fieldName'] ?>" id="mooc_sign_up_<?= $field['fieldName'] ?>"<?= $field['required'] ? ' required' : '' ?>>
                     <option><?=_('--')?></option>
-                    <?php foreach ($field['choices'] as $choice): ?>
-                        <?php $choice = trim($choice) ?>
+                    <? foreach ($field['choices'] as $choice): ?>
+                        <? $choice = trim($choice) ?>
                         <option value="<?=htmlReady($choice)?>"<?=$userInput[$field['fieldName']] == $choice ? ' selected' : ''?>><?=htmlReady($choice)?></option>
-                    <?php endforeach ?>
+                    <? endforeach ?>
                 </select>
-            <?php elseif ($field['type'] === 'textarea'): ?>
+            <? elseif ($field['type'] === 'textarea'): ?>
             <textarea<?= $field['required'] ? ' required' : '' ?>
                 name="<?= $field['fieldName'] ?>"
                 id="mooc_sign_up_<?= $field['fieldName'] ?>"
                 placeholder="<?= $field['label'] ?>"
                 cols="50"
                 rows="10"><?= htmlReady($userInput[$field['fieldName']]) ?></textarea>
-            <?php else: ?>
+            <? else: ?>
             <input type="text"
                 name="<?= $field['fieldName'] ?>"
                 id="mooc_sign_up_<?= $field['fieldName'] ?>"
                 placeholder="<?= $field['label'] ?>"
                 value="<?= htmlReady($userInput[$field['fieldName']]) ?>"<?= $field['required'] ? ' required' : '' ?>>
-            <?php endif ?>
-        <?php else: ?>
+            <? endif ?>
+        <? else: ?>
             <span class="mooc_registration_form_text"><?= $field ?></span>
-        <?php endif ?>
-    <?php endforeach ?>
+        <? endif ?>
+    <? endforeach ?>
 
     <br>
 
