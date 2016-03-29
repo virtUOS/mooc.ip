4eaba45c267582cc19c94cfae0998f314d5096bc
Nils Oesting <noesting@uni-osnabrueck.de>
2016-03-29 10:04:48 +0200

    Fixes  #385

--- migrations/010_add_course_datafields.php
+++ migrations/010_add_course_datafields.php
@@ -13,12 +13,14 @@ class AddCourseDatafields extends Migration
 
     public function up()
     {
+        $mooc_sem_class = Config::get()->getValue(\Mooc\SEM_CLASS_CONFIG_ID);
+
         DBManager::get()->exec(
             "INSERT INTO `datafields` (`datafield_id`, `name`, `object_type`,
                 `object_class`, `edit_perms`, `view_perms`, `priority`,
                 `mkdate`, `chdate`, `type`, `typeparam`, `is_required`, `description`)
             VALUES (md5('(M)OOC Startdatum'), '(M)OOC Startdatum', 1,
-                NULL, 3, 1, '0', NULL, NULL, 5, '', '0', 'Startdatum des (M)OOC Kurses')"
+                $mooc_sem_class, 3, 1, '0', NULL, NULL, 5, '', '0', 'Startdatum des (M)OOC Kurses')"
         );
 
         DBManager::get()->exec(
@@ -26,7 +28,7 @@ class AddCourseDatafields extends Migration
                 `object_class`, `edit_perms`, `view_perms`, `priority`,
                 `mkdate`, `chdate`, `type`, `typeparam`, `is_required`, `description`)
             VALUES (md5('(M)OOC Dauer'), '(M)OOC Dauer', 1,
-                NULL, 3, 1, '0', NULL, NULL, 2, '', '0', 'Dauer des (M)OOC Kurses')"
+                $mooc_sem_class, 3, 1, '0', NULL, NULL, 2, '', '0', 'Dauer des (M)OOC Kurses')"
         );
         
         DBManager::get()->exec(
@@ -34,7 +36,7 @@ class AddCourseDatafields extends Migration
                 `object_class`, `edit_perms`, `view_perms`, `priority`,
                 `mkdate`, `chdate`, `type`, `typeparam`, `is_required`, `description`)
             VALUES (md5('(M)OOC Hinweise'), '(M)OOC Hinweise', 1,
-                NULL, 3, 1, '0', NULL, NULL, 3, '', '0', 'Hinweise für (M)OOC Kurs')"
+                $mooc_sem_class, 3, 1, '0', NULL, NULL, 3, '', '0', 'Hinweise f?r (M)OOC Kurs')"
         );        
     }
 
